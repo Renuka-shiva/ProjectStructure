4ad8a1967d40f7fab3c1d0b0d824245909e103d6
Renuka-shiva <renu.tech123@gmail.com>
2020-08-12 16:01:28 +0530

    First committing

---
+++ src/main/java/com/InetBanking/App.java
@@ -0,0 +1,13 @@
+package com.InetBanking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
